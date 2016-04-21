d4dfb42421e67560e186e9a5ca0ee444d50d76e8
Steve Shim <steveyshim@gmail.com>
2016-04-21 09:02:03 -0700

    added some basic mongo setup to CsvCollector

--- src/main/java/edu/csula/datascience/acquisition/CsvCollector.java
+++ src/main/java/edu/csula/datascience/acquisition/CsvCollector.java
@@ -1,15 +1,29 @@
 package edu.csula.datascience.acquisition;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 import java.util.Collection;
 
 
 public class CsvCollector implements Collector<String, String>{
+    MongoClient mongoClient;
+    MongoDatabase database;
+    MongoCollection<Document> collection;
     public CsvCollector(){
+        // establish database connection to MongoDB
+        mongoClient = new MongoClient();
 
+        database = mongoClient.getDatabase("movie-data");
+
+        // select collection by name `tweets`
+        collection = database.getCollection("csv_files");
     }
 
     public Collection<String> mungee(Collection<String> src){
-        return null;
+        return src;
     }
 
     public void save(Collection<String> data){
