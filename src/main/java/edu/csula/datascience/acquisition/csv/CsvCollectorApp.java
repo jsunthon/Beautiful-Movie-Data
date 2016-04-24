41e7388eca3c8fb5c0a4cfa53555b61608a177dc
jsunthon <jsunthon@gmail.com>
2016-04-24 16:00:18 -0700

    merging

--- src/main/java/edu/csula/datascience/acquisition/csv/CsvCollectorApp.java
+++ src/main/java/edu/csula/datascience/acquisition/csv/CsvCollectorApp.java
@@ -1,6 +1,7 @@
 package edu.csula.datascience.acquisition.csv;
 
 
+import edu.csula.datascience.acquisition.Movie;
 import edu.csula.datascience.utilities.MongoUtilities;
 
 import java.util.ArrayList;
