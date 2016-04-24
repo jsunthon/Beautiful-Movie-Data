1897e58ef854f905d6383bd7e0492abd404ae2a9
steveshim <steveyshim@gmail.com>
2016-04-24 13:59:11 -0700

    validates title before adding to mongo

--- src/main/java/edu/csula/datascience/acquisition/CsvCollector.java
+++ src/main/java/edu/csula/datascience/acquisition/CsvCollector.java
@@ -17,6 +17,8 @@ public class CsvCollector implements Collector<Movie, Movie>{
     MongoClient mongoClient;
     MongoDatabase database;
     MongoCollection<Document> collection;
+    private String title = "";
+
     public CsvCollector(){
         // establish database connection to MongoDB
         mongoClient = new MongoClient();
@@ -32,26 +34,31 @@ public class CsvCollector implements Collector<Movie, Movie>{
         ArrayList<Movie> srcArray = new ArrayList(src);
         ArrayList<Movie> finalMovieEntry = new ArrayList();
         double averageRating = 0.0;
-        String title = srcArray.get(0).getTitle();
+        title = srcArray.get(0).getTitle();
         int year = 0;
 
         //regex found on stackoverflow
         Matcher m = Pattern.compile("\\(([^)]+)\\)").matcher(title);
         while(m.find()) {
-            year = Integer.parseInt(m.group(1));
+            try {
+                year = Integer.parseInt(m.group(1));
+            } catch (Exception e){
+                //if there is a parenthesis in the title
+            }
             title = title.replaceAll("\\(.*?\\) ?", "");
         }
 
         Movie theMovie = new Movie(srcArray.get(0).getId(), title);
-        for (Movie movie : srcArray){
+        for (Movie movie : srcArray) {
             averageRating += movie.getRating();
         }
-        averageRating =  averageRating/(double)src.size();
+        averageRating = averageRating / (double) src.size();
         theMovie.setRating(averageRating);
         theMovie.setYear(year);
         finalMovieEntry.add(theMovie);
 
         return finalMovieEntry;
+
     }
 
     public void save(Collection<Movie> data){
@@ -67,9 +74,7 @@ public class CsvCollector implements Collector<Movie, Movie>{
 
     }
 
-    private boolean isValidTitle(String title){
-        boolean valid = true;
-        valid = title.matches("\\w+");
-        return valid;
-    }
+
+
+
 }
--- src/main/java/edu/csula/datascience/acquisition/CsvCollectorApp.java
+++ src/main/java/edu/csula/datascience/acquisition/CsvCollectorApp.java
@@ -3,6 +3,7 @@ package edu.csula.datascience.acquisition;
 
 import edu.csula.datascience.utilities.MongoUtilities;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -17,11 +18,25 @@ public class CsvCollectorApp {
         while (source.hasNext()){
             Collection<Movie> movies = source.next();
             Collection<Movie> mungedMovies = collector.mungee(movies);
-            collector.save(mungedMovies);
+            if (isValidTitle(new ArrayList<Movie>(mungedMovies).get(0).getTitle())){
+                collector.save(mungedMovies);
+            }
+            else{
+                continue;
+            }
+
         }
 
         MongoUtilities mongoUtil = new MongoUtilities("movie-data", "csv_files");
         mongoUtil.printDocuments();
 
+
+    }
+
+    //regex from StackOverflow
+    private static boolean isValidTitle(String title){
+        boolean valid = title.matches("^[a-zA-Z0-9_ ]*$");
+
+        return valid;
     }
 }
