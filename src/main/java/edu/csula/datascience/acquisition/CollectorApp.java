9042179e671eba0e8e9259b3fa4ccb63c0c6f0dc
jsunthon <jsunthon@gmail.com>
2016-04-27 11:27:09 -0700

    some code changes; set streamDuration to 1 minute to avoid rate limit

--- src/main/java/edu/csula/datascience/acquisition/CollectorApp.java
+++ src/main/java/edu/csula/datascience/acquisition/CollectorApp.java
@@ -5,55 +5,51 @@ import edu.csula.datascience.acquisition.csv.CsvSource;
 import edu.csula.datascience.acquisition.twitter.TwitterCollector;
 import edu.csula.datascience.acquisition.twitter.TwitterResponse;
 import edu.csula.datascience.acquisition.twitter.TwitterSource;
-import edu.csula.datascience.utilities.MongoUtilities;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 public class CollectorApp {
-    public static void main(String[] args){
-        boolean firstRun = true;
+	public static void main(String[] args) throws InterruptedException {
+		
+		/**
+		 * SET THE TIME LENGTH YOU WANT TO LISTEN TO EACH MOVIE. In ms!
+		 */
+		long streamDuration = 60000;
 
-        while(true) { //will run through all titles to continuously go through live twitter feed
-            //TODO add file
-            String file = "mergedMovieData.csv";
-            CsvSource source = new CsvSource(file, true);
-            CsvCollector collector = new CsvCollector();
+		String file = "mergedMovieData.csv";
+		CsvSource source = new CsvSource(file, true);
+		CsvCollector collector = new CsvCollector();
 
-            while (source.hasNext()) {
-                Collection<Movie> movies = source.next();
-                Collection<Movie> mungedMovies = collector.mungee(movies);
-                ArrayList<Movie> munged = new ArrayList(mungedMovies);
-                if (!mungedMovies.isEmpty() && munged.get(0).getYear() > 2010) {
-                    if(firstRun) {
-                        //only saves csv data on first run, used to continuously run through twitter
-                        collector.save(mungedMovies);
-                    }
+		while (source.hasNext()) {
+			Collection<Movie> movies = source.next();
+			Collection<Movie> mungedMovies = collector.mungee(movies);
+			ArrayList<Movie> munged = new ArrayList(mungedMovies);
 
-                    TwitterSource tsource = new TwitterSource(munged.get(0).getHashtagTitle(), 10000);
-                    TwitterCollector tcollector = new TwitterCollector();
-                    Set<TwitterResponse> initResponses = new HashSet<TwitterResponse>();
-
-                    while (tsource.hasNext()) {
-                        Collection<TwitterResponse> tweets = tsource.next();
-                        if (tweets.size() != 0) {
-                            initResponses.addAll(tweets);
-                        }
-                    }
-                    tsource.stopStream();
-                    Collection<TwitterResponse> cleanedTweets = tcollector.mungee(initResponses);
-                    tcollector.save(cleanedTweets);
-                } else {
-                    continue;
-                }
-
-            }
-            firstRun = false;
-        }
-        //MongoUtilities mongoUtil = new MongoUtilities("movie-data", "csv_files");
-        //mongoUtil.printDocuments();
-
-    }
+			if (!mungedMovies.isEmpty()) {
+				System.out.println("NOT SAVED: munged movie of YEAR: " + munged.get(0).getYear());
+				if (munged.get(0).getYear() > 2010) {
+					collector.save(mungedMovies);
+					TwitterSource tsource = new TwitterSource(munged.get(0).getHashtagTitle(), streamDuration);
+					TwitterCollector tcollector = new TwitterCollector();
+					Set<TwitterResponse> initResponses = new HashSet<TwitterResponse>();
+					System.out.println(
+							"size of SAVED munged " + munged.size() + " and year is " + munged.get(0).getYear());
+					while (tsource.hasNext()) {
+						Collection<TwitterResponse> tweets = tsource.next();
+						if (tweets.size() != 0) {
+							initResponses.addAll(tweets);
+						}
+					}
+					tsource.stopStream();
+					tsource = null;
+					Collection<TwitterResponse> cleanedTweets = tcollector.mungee(initResponses);
+					tcollector.save(cleanedTweets);
+					System.out.println("Sleeping the thread...");
+					Thread.sleep(10000);
+				}
+			}
+		}
+	}
 }
