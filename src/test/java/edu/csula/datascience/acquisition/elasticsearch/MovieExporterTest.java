7c24ed00194a2706938951c529d626cc39dcb85a
jsunthon <jsunthon@gmail.com>
2016-06-11 15:43:55 -0700

    add code to export final movie data to .json for web visualization

--- src/test/java/edu/csula/datascience/acquisition/elasticsearch/MovieExporterTest.java
+++ src/test/java/edu/csula/datascience/acquisition/elasticsearch/MovieExporterTest.java
@@ -2,14 +2,21 @@ package edu.csula.datascience.acquisition.elasticsearch;
 
 import static org.junit.Assert.*;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.google.gson.Gson;
+
 import edu.csula.datascience.elasticsearch.MovieExporter;
 import edu.csula.datascience.elasticsearch.MongoMovieImporter;
 import edu.csula.datascience.elasticsearch.model.Movie;
@@ -27,37 +34,58 @@ public class MovieExporterTest {
 		movies = movieDbHelper.getMovies();
 		mockMovies = new ArrayList<>();
 		mockMovies = MovieExporterTestHelpers.generateMockMovies();
-		exporter = new MovieExporter("darkserith", movies, 4);
+//		exporter = new MovieExporter("darkserith", movies, 4);
 	}
 	
-	@Test
-	public void testExport() {
-		exporter.exportMovies(mockMovies);
-	}
+//	@Test
+//	public void testExport() {
+//		exporter.exportMovies(mockMovies);
+//	}
+//	
+//	@Test
+//	public void testMovie() {
+//		String testStr = "Hello Love Me Not...";
+//		assertEquals(true, testStr.contains("#Love ") || testStr.contains("Love "));
+//	}
+//		
+//	@SuppressWarnings("deprecation")
+//	@Test
+//	public void validateSentimentTxtFile() {
+//		List<String> positiveWords = new ArrayList<>();
+//		List<String> negativeWords = new ArrayList<>();		
+//		try {
+//			positiveWords = Files.readAllLines(Paths.get(ClassLoader.getSystemResource("positive-words.txt").toURI()));
+//			negativeWords = Files.readAllLines(Paths.get(ClassLoader.getSystemResource("negative-words.txt").toURI()));
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		} catch (URISyntaxException e) {
+//			e.printStackTrace();
+//		}
+//		assertTrue(positiveWords.size() > 0 && negativeWords.size() > 0);
+//		assertEquals("a+", positiveWords.get(0));
+//		assertEquals("2-faced", negativeWords.get(0));		
+//		double sentiment = 0;		
+//		sentiment = (double) 300 / 1000;
+//	}
 	
 	@Test
-	public void testMovie() {
-		String testStr = "Hello Love Me Not...";
-		assertEquals(true, testStr.contains("#Love ") || testStr.contains("Love "));
-	}
+	public void testMoviesToJson() throws IOException {
+
+			//at this pt, we have the movie data we want. output to json.
+			JSONObject obj = new JSONObject();
+			obj.put("Name", "StaticMovieJson");
+			JSONArray moviesJsonArr = new JSONArray();
+			for (Movie movie : mockMovies) {
+				moviesJsonArr.add(new Gson().toJson(movie));
+			}
+			obj.put("Movies", moviesJsonArr);
+	 
+			// try-with-resources statement based on post comment below :)
+			try (FileWriter file = new FileWriter("/Users/James/Documents/movies.json")) {
+				file.write(obj.toJSONString());
+				System.out.println("Successfully Copied JSON Object to File...");
+				System.out.println("\nJSON Object: " + obj);
+			}
 		
-	@SuppressWarnings("deprecation")
-	@Test
-	public void validateSentimentTxtFile() {
-		List<String> positiveWords = new ArrayList<>();
-		List<String> negativeWords = new ArrayList<>();		
-		try {
-			positiveWords = Files.readAllLines(Paths.get(ClassLoader.getSystemResource("positive-words.txt").toURI()));
-			negativeWords = Files.readAllLines(Paths.get(ClassLoader.getSystemResource("negative-words.txt").toURI()));
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		}
-		assertTrue(positiveWords.size() > 0 && negativeWords.size() > 0);
-		assertEquals("a+", positiveWords.get(0));
-		assertEquals("2-faced", negativeWords.get(0));		
-		double sentiment = 0;		
-		sentiment = (double) 300 / 1000;
 	}
 }
